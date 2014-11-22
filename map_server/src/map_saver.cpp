ae34f850ca238e86f85111d293d28b621d04d3f2
Marcus Liebhardt <ich@marcusliebhardt.de>
2014-11-22 14:09:34 +0900

    applies ros formatting

--- map_server/src/map_saver.cpp
+++ map_server/src/map_saver.cpp
@@ -36,88 +36,91 @@
 #include "geometry_msgs/Quaternion.h"
 
 using namespace std;
- 
+
 /**
  * @brief Map generation node.
  */
-class MapGenerator 
+class MapGenerator
 {
 
-  public:
-    MapGenerator(const std::string& mapname) : mapname_(mapname), saved_map_(false)
+public:
+  MapGenerator(const std::string& mapname) :
+      mapname_(mapname), saved_map_(false)
+  {
+    ros::NodeHandle n;
+    ROS_INFO("Waiting for the map");
+    map_sub_ = n.subscribe("map", 1, &MapGenerator::mapCallback, this);
+  }
+
+  void mapCallback(const nav_msgs::OccupancyGridConstPtr& map)
+  {
+    ROS_INFO("Received a %d X %d map @ %.3f m/pix", map->info.width, map->info.height, map->info.resolution);
+
+    std::string mapdatafile = mapname_ + ".pgm";
+    ROS_INFO("Writing map occupancy data to %s", mapdatafile.c_str());
+    FILE* out = fopen(mapdatafile.c_str(), "w");
+    if (!out)
     {
-      ros::NodeHandle n;
-      ROS_INFO("Waiting for the map");
-      map_sub_ = n.subscribe("map", 1, &MapGenerator::mapCallback, this);
+      ROS_ERROR("Couldn't save map file to %s", mapdatafile.c_str());
+      return;
     }
 
-    void mapCallback(const nav_msgs::OccupancyGridConstPtr& map)
+    fprintf(out, "P5\n# CREATOR: Map_generator.cpp %.3f m/pix\n%d %d\n255\n", map->info.resolution, map->info.width,
+            map->info.height);
+    for (unsigned int y = 0; y < map->info.height; y++)
     {
-      ROS_INFO("Received a %d X %d map @ %.3f m/pix",
-               map->info.width,
-               map->info.height,
-               map->info.resolution);
-
-
-      std::string mapdatafile = mapname_ + ".pgm";
-      ROS_INFO("Writing map occupancy data to %s", mapdatafile.c_str());
-      FILE* out = fopen(mapdatafile.c_str(), "w");
-      if (!out)
+      for (unsigned int x = 0; x < map->info.width; x++)
       {
-        ROS_ERROR("Couldn't save map file to %s", mapdatafile.c_str());
-        return;
-      }
-
-      fprintf(out, "P5\n# CREATOR: Map_generator.cpp %.3f m/pix\n%d %d\n255\n",
-              map->info.resolution, map->info.width, map->info.height);
-      for(unsigned int y = 0; y < map->info.height; y++) {
-        for(unsigned int x = 0; x < map->info.width; x++) {
-          unsigned int i = x + (map->info.height - y - 1) * map->info.width;
-          if (map->data[i] == 0) { //occ [0,0.1)
-            fputc(254, out);
-          } else if (map->data[i] == +100) { //occ (0.65,1]
-            fputc(000, out);
-          } else { //occ [0.1,0.65]
-            fputc(205, out);
-          }
+        unsigned int i = x + (map->info.height - y - 1) * map->info.width;
+        if (map->data[i] == 0)
+        { //occ [0,0.1)
+          fputc(254, out);
+        }
+        else if (map->data[i] == +100)
+        { //occ (0.65,1]
+          fputc(000, out);
+        }
+        else
+        { //occ [0.1,0.65]
+          fputc(205, out);
         }
       }
-
-      fclose(out);
-
-
-      std::string mapmetadatafile = mapname_ + ".yaml";
-      ROS_INFO("Writing map occupancy data to %s", mapmetadatafile.c_str());
-      FILE* yaml = fopen(mapmetadatafile.c_str(), "w");
-
-
-      /*
-resolution: 0.100000
-origin: [0.000000, 0.000000, 0.000000]
-#
-negate: 0
-occupied_thresh: 0.65
-free_thresh: 0.196
-
-       */
-
-      geometry_msgs::Quaternion orientation = map->info.origin.orientation;
-      tf::Matrix3x3 mat(tf::Quaternion(orientation.x, orientation.y, orientation.z, orientation.w));
-      double yaw, pitch, roll;
-      mat.getEulerYPR(yaw, pitch, roll);
-
-      fprintf(yaml, "image: %s\nresolution: %f\norigin: [%f, %f, %f]\nnegate: 0\noccupied_thresh: 0.65\nfree_thresh: 0.196\n\n",
-              mapdatafile.c_str(), map->info.resolution, map->info.origin.position.x, map->info.origin.position.y, yaw);
-
-      fclose(yaml);
-
-      ROS_INFO("Done\n");
-      saved_map_ = true;
     }
 
-    std::string mapname_;
-    ros::Subscriber map_sub_;
-    bool saved_map_;
+    fclose(out);
+
+    std::string mapmetadatafile = mapname_ + ".yaml";
+    ROS_INFO("Writing map occupancy data to %s", mapmetadatafile.c_str());
+    FILE* yaml = fopen(mapmetadatafile.c_str(), "w");
+
+    /*
+     resolution: 0.100000
+     origin: [0.000000, 0.000000, 0.000000]
+     #
+     negate: 0
+     occupied_thresh: 0.65
+     free_thresh: 0.196
+
+     */
+
+    geometry_msgs::Quaternion orientation = map->info.origin.orientation;
+    tf::Matrix3x3 mat(tf::Quaternion(orientation.x, orientation.y, orientation.z, orientation.w));
+    double yaw, pitch, roll;
+    mat.getEulerYPR(yaw, pitch, roll);
+
+    fprintf(yaml,
+            "image: %s\nresolution: %f\norigin: [%f, %f, %f]\nnegate: 0\noccupied_thresh: 0.65\nfree_thresh: 0.196\n\n",
+            mapdatafile.c_str(), map->info.resolution, map->info.origin.position.x, map->info.origin.position.y, yaw);
+
+    fclose(yaml);
+
+    ROS_INFO("Done\n");
+    saved_map_ = true;
+  }
+
+  std::string mapname_;
+  ros::Subscriber map_sub_;
+  bool saved_map_;
 
 };
 
@@ -125,21 +128,21 @@ free_thresh: 0.196
               "  map_saver -h\n"\
               "  map_saver [-f <mapname>] [ROS remapping args]"
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
   ros::init(argc, argv, "map_saver");
   std::string mapname = "map";
 
-  for(int i=1; i<argc; i++)
+  for (int i = 1; i < argc; i++)
   {
-    if(!strcmp(argv[i], "-h"))
+    if (!strcmp(argv[i], "-h"))
     {
       puts(USAGE);
       return 0;
     }
-    else if(!strcmp(argv[i], "-f"))
+    else if (!strcmp(argv[i], "-f"))
     {
-      if(++i < argc)
+      if (++i < argc)
         mapname = argv[i];
       else
       {
@@ -153,13 +156,12 @@ int main(int argc, char** argv)
       return 1;
     }
   }
-  
+
   MapGenerator mg(mapname);
 
-  while(!mg.saved_map_)
+  while (!mg.saved_map_)
     ros::spinOnce();
 
   return 0;
 }
-
 
